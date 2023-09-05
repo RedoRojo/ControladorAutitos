0e9b63595805627b611cd65d257f153565fcbf0e
RedoRojo <cristhiabnvictorrm@gmail.com>
2023-09-05 07:25:14 -0400

    Se ejecuta toda la secuencia de comandos y se muestra la posicion final

--- src/presenter.js
+++ src/presenter.js
@@ -11,7 +11,7 @@ const comando = document.querySelector("#comando-button");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  controlador = new Controlador(); 
+  controlador = new Controlador(comandos.value); 
 
   coordenada_inicial = controlador.obtenerCoordenadaInicial(comandos.value); 
   secuencia = controlador.obtenerSecuenciaDeComandos(comandos.value); 
@@ -34,34 +34,6 @@ form.addEventListener("submit", (event) => {
   } else { 
     alert("Ingrese unas dimesiones para la superficie validas"); 
   }
+
+  pos_final.innerHTML = controlador.ejecutarSecuencia(); 
 });
-
-comando.addEventListener("click", () => {
-  controlador = new Controlador(comandos.value); 
-
-  coordenada_inicial = controlador.obtenerCoordenadaInicial(comandos.value);
-  secuencia = controlador.obtenerSecuenciaDeComandos(comandos.value); 
-  dimensiones = controlador.obtenerDimensiones(comandos.value); 
-
-  if(controlador.validarCoordenada(coordenada_inicial))  {
-    pos_inicial.innerHTML = coordenada_inicial; 
-  }else {
-    alert("Ingrese una posicion inicial correcta"); 
-  }
-
-  if(controlador.validarSecuencia(secuencia)) {
-    sec_comandos.innerHTML = secuencia; 
-  } else {
-    alert("ingrese una secuencia de comandos correcta"); 
-  }
-
-  if(controlador.validarDimensiones(dimensiones)) {
-    dimen.innerHTML = dimensiones; 
-  } else { 
-    alert("Ingrese unas dimesiones para la superficie validas"); 
-  }
-
-  final = controlador.ejecutarComandoA(coordenada_inicial); 
-  pos_final.innerHTML = final; 
-
-})
