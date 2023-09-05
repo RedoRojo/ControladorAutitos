2880c2cebcf82cfdf858a3f0e7ce9d91204a5744
RedoRojo <cristhiabnvictorrm@gmail.com>
2023-09-04 21:37:35 -0400

    Se valida que la coordenada 1923,32B no es valida

--- src/controlador.test.js
+++ src/controlador.test.js
@@ -81,4 +81,9 @@ describe("Coordena Inicial", () => {
     expect(controlador.validarCoordenada("3,3,324")).toEqual(false); 
   }); 
 
+  it("Deberia retornar false al validar la coordenada: 1923,32B", () => {
+    let controlador = new Controlador();
+    expect(controlador.validarCoordenada("1923,32B")).toEqual(false); 
+  }); 
+
 });
